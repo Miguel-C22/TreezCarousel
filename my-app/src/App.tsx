e03948258375b644a5ae554c3f45f6774f2e278c
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-07-04 16:01:10 -0500

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,9 @@ import Drawer from "./components/DrawerCatagories";
 function App() {
   return (
     <div className="App">
+      <div className="logo-container">
+        <img className="logo" src="/images/Treez-Logo.png" alt="" />
+      </div>
       <Carousel />
     </div>
   );
